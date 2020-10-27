d6db0b0bc875ee6ce46c0016c1ef34f2a3e5c949
Masaaki Goshima <goccy54@gmail.com>
2020-10-27 15:10:31 +0900

    Add examples dir

---
+++ _examples/go_test.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_A(t *testing.T) {
+	t.Log("Test A")
+	time.Sleep(time.Second)
+}
+
+func Test_B(t *testing.T) {
+	t.Log("Test B")
+	time.Sleep(time.Second)
+}
+
+func Test_C(t *testing.T) {
+	t.Log("Test C")
+	time.Sleep(time.Second)
+}
+
+func Test_D(t *testing.T) {
+	t.Log("Test D")
+	time.Sleep(time.Second)
+}
+
+func Test_E(t *testing.T) {
+	t.Log("Test E")
+	time.Sleep(time.Second)
+}
+
+func Test_F(t *testing.T) {
+	t.Log("Test F")
+	time.Sleep(time.Second)
+}
+
+func Test_G(t *testing.T) {
+	t.Log("Test G")
+	time.Sleep(time.Second)
+}
