ab4be3e12fc21ad8de71c1eb2556aa95e8142feb
Masaaki Goshima <goccy54@gmail.com>
2020-12-18 19:09:35 +0900

    Add test cases

--- api/v1/kubetest_test.go
+++ api/v1/kubetest_test.go
@@ -24,7 +24,8 @@ func init() {
 }
 
 func Test_Run(t *testing.T) {
-	crd := `
+	t.Run("checkout branch", func(t *testing.T) {
+		crd := `
 apiVersion: kubetest.io/v1
 kind: TestJob
 metadata:
@@ -62,17 +63,121 @@ spec:
         - .
       pattern: ^Test
 `
-	runner, err := kubetestv1.NewTestJobRunner(cfg)
-	if err != nil {
-		t.Fatalf("%+v", err)
-	}
-	var job kubetestv1.TestJob
-	if err := yaml.NewYAMLOrJSONDecoder(strings.NewReader(crd), 1024).Decode(&job); err != nil {
-		t.Fatalf("%+v", err)
-	}
-	if err := runner.Run(context.Background(), job); err != nil {
-		t.Fatalf("%+v", err)
-	}
+		runner, err := kubetestv1.NewTestJobRunner(cfg)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+		var job kubetestv1.TestJob
+		if err := yaml.NewYAMLOrJSONDecoder(strings.NewReader(crd), 1024).Decode(&job); err != nil {
+			t.Fatalf("%+v", err)
+		}
+		if err := runner.Run(context.Background(), job); err != nil {
+			t.Fatalf("%+v", err)
+		}
+	})
+	t.Run("merge base branch", func(t *testing.T) {
+		crd := `
+apiVersion: kubetest.io/v1
+kind: TestJob
+metadata:
+  name: testjob
+  namespace: default
+spec:
+  git:
+    repo: github.com/goccy/kubetest
+    base: master
+    branch: master
+    checkoutDir: /go/src/kubetest
+  template:
+    spec:
+      containers:
+        - name: test
+          image: golang:1.15
+          command:
+            - go
+          args:
+            - test
+            - -v
+            - ./
+            - -run
+            - $TEST
+          workingDir: /go/src/kubetest/_examples/success
+  distributedTest:
+    containerName: test
+    maxContainersPerPod: 2
+    maxConcurrentNumPerPod: 1
+    list:
+      command:
+        - go
+      args:
+        - test
+        - -list
+        - .
+      pattern: ^Test
+`
+		runner, err := kubetestv1.NewTestJobRunner(cfg)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+		var job kubetestv1.TestJob
+		if err := yaml.NewYAMLOrJSONDecoder(strings.NewReader(crd), 1024).Decode(&job); err != nil {
+			t.Fatalf("%+v", err)
+		}
+		if err := runner.Run(context.Background(), job); err != nil {
+			t.Fatalf("%+v", err)
+		}
+	})
+	t.Run("retest", func(t *testing.T) {
+		crd := `
+apiVersion: kubetest.io/v1
+kind: TestJob
+metadata:
+  name: testjob
+  namespace: default
+spec:
+  git:
+    repo: github.com/goccy/kubetest
+    branch: master
+    checkoutDir: /go/src/kubetest
+  template:
+    spec:
+      containers:
+        - name: test
+          image: golang:1.15
+          command:
+            - go
+          args:
+            - test
+            - -v
+            - ./
+            - -run
+            - $TEST
+          workingDir: /go/src/kubetest/_examples/failure
+  distributedTest:
+    containerName: test
+    maxContainersPerPod: 2
+    retest: true
+    list:
+      command:
+        - go
+      args:
+        - test
+        - -list
+        - .
+      pattern: ^Test
+`
+		runner, err := kubetestv1.NewTestJobRunner(cfg)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+		var job kubetestv1.TestJob
+		if err := yaml.NewYAMLOrJSONDecoder(strings.NewReader(crd), 1024).Decode(&job); err != nil {
+			t.Fatalf("%+v", err)
+		}
+		if err := runner.Run(context.Background(), job); err == nil {
+			t.Fatal("expected error")
+		}
+	})
 }
 
 func Test_RunWithDebugLog(t *testing.T) {
