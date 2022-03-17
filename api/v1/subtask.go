b79db41c71acb991a093328554adcf0cd4f1ec5b
Masaaki Goshima <goccy54@gmail.com>
2022-03-17 16:53:34 +0900

    add termination log

--- api/v1/subtask.go
+++ api/v1/subtask.go
@@ -46,11 +46,18 @@ func (t *SubTask) outputError(logGroup Logger, baseErr error) {
 	}
 }
 
+const (
+	terminationLog = "kubetest task is completed"
+)
+
 func (t *SubTask) Run(ctx context.Context) *SubTaskResult {
 	logger := LoggerFromContext(ctx)
 	logGroup := logger.Group()
 	ctx = WithLogger(ctx, logGroup)
 	defer func() {
+		if err := t.exec.TerminationLog(ctx, terminationLog); err != nil {
+			logGroup.Warn("failed to send termination log: %s", err.Error())
+		}
 		logger.LogGroup(logGroup)
 		if t.OnFinish != nil {
 			t.OnFinish(t)
