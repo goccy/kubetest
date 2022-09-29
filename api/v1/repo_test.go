7c13873f19d7b2602330bf56316d5d274f2672da
zoncoen <zoncoen@gmail.com>
2022-09-29 14:25:02 +0900

    Add workaround for a bug of go-git concerned with .gitignore

--- api/v1/repo_test.go
+++ api/v1/repo_test.go
@@ -2,10 +2,21 @@ package v1
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
 	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/go-git/go-billy/v5"
+	"github.com/go-git/go-billy/v5/osfs"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/cache"
+	"github.com/go-git/go-git/v5/storage/filesystem"
+	"github.com/sosedoff/gitkit"
 )
 
 func TestRepositoryManager(t *testing.T) {
@@ -105,4 +116,163 @@ func TestRepositoryManager(t *testing.T) {
 		}
 		t.Logf("archive path: %s", path)
 	})
+	t.Run("add a file that specified to be ignored on the base branch", func(t *testing.T) {
+		addr, reposDir := runGitServer(t)
+
+		// create test repository
+		repoName := "test"
+		fs := osfs.New(filepath.Join(reposDir, repoName))
+		storage := filesystem.NewStorage(fs, cache.NewObjectLRUDefault())
+		repo, err := git.Init(storage, fs)
+		if err != nil {
+			t.Fatal(err)
+		}
+		w, err := repo.Worktree()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// create .gitignore
+		f, err := fs.Create(".gitignore")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+		if _, err := f.Write([]byte("*.txt")); err != nil {
+			t.Fatal(err)
+		}
+		if _, err := w.Add(".gitignore"); err != nil {
+			t.Fatal(err)
+		}
+
+		// commit1
+		commit1, err := w.Commit("commit1", &git.CommitOptions{})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// update .gitignore
+		f, err = fs.OpenFile(".gitignore", os.O_RDWR|os.O_APPEND, 0o666)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if _, err := f.Write([]byte("\n!test.txt")); err != nil {
+			t.Fatal(err)
+		}
+
+		// create test.txt
+		f, err = fs.Create("test.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+		if _, err := f.Write([]byte("test")); err != nil {
+			t.Fatal(err)
+		}
+		if _, err := w.Add("test.txt"); err != nil {
+			t.Fatal(err)
+		}
+
+		// commit2
+		commit2, err := w.Commit("commit2", &git.CommitOptions{All: true})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// set refs/heads/master => commit1
+		master := plumbing.NewHashReference("refs/heads/master", commit1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := repo.Storer.SetReference(master); err != nil {
+			t.Fatal(err)
+		}
+
+		// set refs/heads/feature => commit2
+		feature := plumbing.NewHashReference("refs/heads/feature", commit2)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := repo.Storer.SetReference(feature); err != nil {
+			t.Fatal(err)
+		}
+
+		// clone by RepositoryManager
+		repoDir := filepath.Join(t.TempDir(), repoName)
+		spec := RepositorySpec{
+			Name: repoName,
+			Value: Repository{
+				URL: fmt.Sprintf("http://%s/%s", addr, repoName),
+				Rev: feature.Hash().String(),
+				Merge: &MergeSpec{
+					Base: "master",
+				},
+				ClonedPath: repoDir,
+			},
+		}
+		if err := NewValidator().ValidateRepositorySpec(spec); err != nil {
+			t.Fatal(err)
+		}
+		mgr := NewRepositoryManager([]RepositorySpec{spec}, new(TokenManager))
+		t.Cleanup(func() {
+			mgr.Cleanup()
+		})
+		if err := mgr.CloneAll(WithLogger(context.Background(), NewLogger(os.Stdout, LogLevelDebug))); err != nil {
+			t.Fatal(err)
+		}
+
+		repo, err = git.PlainOpen(repoDir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		w, err = repo.Worktree()
+		if err != nil {
+			t.Fatal(err)
+		}
+		assertFile(t, w.Filesystem, ".gitignore", "*.txt\n!test.txt")
+		assertFile(t, w.Filesystem, "test.txt", "test")
+	})
+}
+
+func runGitServer(t *testing.T) (string, string) {
+	t.Helper()
+
+	tempDir := t.TempDir()
+	h := gitkit.New(gitkit.Config{
+		Dir: tempDir,
+	})
+	if err := h.Setup(); err != nil {
+		t.Fatal(err)
+	}
+	srv := &http.Server{
+		Handler: h,
+	}
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		_ = srv.Serve(ln)
+	}()
+	t.Cleanup(func() {
+		_ = srv.Close()
+	})
+
+	return ln.Addr().String(), tempDir
+}
+
+func assertFile(t *testing.T, fs billy.Filesystem, path string, expect string) {
+	t.Helper()
+
+	f, err := fs.Open(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	b, err := io.ReadAll(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got := string(b); got != expect {
+		t.Errorf("%s: expect %q but got %q", path, expect, got)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/bradleyfalzon/ghinstallation/v2 v2.0.4
+	github.com/go-git/go-billy/v5 v5.3.1
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/go-logr/logr v0.4.0
 	github.com/goccy/kubejob v0.3.2
@@ -12,6 +13,7 @@ require (
 	github.com/lestrrat-go/backoff v1.0.1
 	github.com/onsi/ginkgo v1.16.2
 	github.com/onsi/gomega v1.12.0
+	github.com/sosedoff/gitkit v0.3.0
 	golang.org/x/mod v0.3.1-0.20200828183125-ce943fd02449 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	golang.org/x/tools v0.1.0 // indirect
