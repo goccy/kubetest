1d47183777ee24885027f99d5369b93faf7375fb
Masaaki Goshima <goccy54@gmail.com>
2023-08-17 12:27:56 +0900

    update dependent libraries

--- api/v1/token.go
+++ api/v1/token.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 
 	"github.com/bradleyfalzon/ghinstallation/v2"
-	"github.com/google/go-github/v29/github"
+	"github.com/google/go-github/v54/github"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
